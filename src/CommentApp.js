1e439a7a5461ed890cf3b84a1e2eea4f9f57659e
周华健 <zhouhuajian@ly.com>
2018-01-04 15:32:29 +0800

    sd

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,38 +1,32 @@
-import React,{Component} from 'react';
+import React,{Componen,PropTypes} from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
+import wrapWithLoadData from './wrapWithLoadData';
 
 class CommentApp extends Component{
-    constructor(){
-        super()
-        this.state = {
-            comments:[]
-        }
+    static propTypes = {
+        data:PropTypes.any,
+        saveData:PropTypes.func.isRequired
     }
-    componentWillMount(){
-        this._loadComments();
+    
+    constructor(props){
+        super(props);
+        this.state = {comments:props.data}
     }
-    _loadComments(){
-        let comments = localStorage.getItem('comments');
-
-        if(comments){
-            comments = JSON.parse(comments);
-            this.setState({comments})
-        }
-    }
-    _saveComments(comments){
-        localStorage.setItem('comments',JSON.stringify(comments))
-    }
+    
     handleSubmitComment(comment){
-        if(!comment)return;
-        if(!comment.username) return alert('请输入用户名');
-        if(!comment.content) return alert('请输入评论内容')
         const comments = this.state.comments;
         comments.push(comment);
         this.setState({
             comments
         })
-        this._saveComments(comments)
+        this.props.saveData(comments)
+    }
+    handleDeleteComment (index){
+        const comments = this.state.comments;
+        comments.splice(index,1);
+        this.setState({comments});
+        this.props.saveData(comments);
     }
     render(){
         return(
